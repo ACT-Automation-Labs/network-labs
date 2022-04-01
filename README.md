5f6d96906530926d65ccdb4bc78211c2df5c7135
rishasi <to.rishasi@gmail.com>
2022-04-01 11:26:05 +0530

    Moving to Github

---
+++ README.md
@@ -0,0 +1,86 @@
+[TOC]
+
+## Introduction
+
+This Repo will act as a base for setting up Networking Labs.
+
+This script and all commands have been tested in Azure CloudShell. Please prefer it over other shells for this LAB.
+
+## Getting Started
+
+To use the scripts in your environment, clone the repository:
+
+```bash
+$ git clone https://AKSTechProLabs@dev.azure.com/AKSTechProLabs/AKSNetworkLabSetup/_git/AKSNetworkLabSetup
+Cloning into 'AKSNetworkLabSetup'...
+Password for 'https://AKSTechProLabs@dev.azure.com':
+```
+
+..give executable access to the scripts and start them. For example:
+
+```bash
+$ cd AKSNetworkLabSetup/
+$ chmod +x setup-lab-1.sh
+$ sh setup-lab-1.sh
+```
+
+Once the Script completes, the following resources are created:
+
+- 2 VNets
+- 2 AKS Clusters
+- 2 Subnets and 2 NSGs
+- 2 Virtual Machines using the above Subnets and NSGs
+
+Verify the resources and find out the relation between them.
+
+Maybe use VNet Topology: https://docs.microsoft.com/en-us/azure/network-watcher/view-network-topology
+
+SSH to the VMs using their Public IP.
+
+```
+User:		azureuser
+Pass:		Passw0rd@123
+```
+
+Please reset the password once you have access to the VM.
+
+Access the Clusters one by one, and apply the Deployment and Service Files:
+
+```bash
+## Create Deployment
+$ kubectl apply -f https://raw.githubusercontent.com/rishasi/udp-client-server/main/k8s-yaml/server-deployment.yaml
+
+## Create Service
+$ kubectl apply -f https://raw.githubusercontent.com/rishasi/udp-client-server/main/k8s-yaml/server-ilb-service.yaml
+```
+
+Verify that the resources are created
+
+```bash
+$ kubectl get deploy,svc
+NAME                         READY   UP-TO-DATE   AVAILABLE   AGE
+deployment.apps/udp-server   4/4     4            4           52s
+
+NAME                 TYPE           CLUSTER-IP    EXTERNAL-IP   PORT(S)           AGE
+service/kubernetes   ClusterIP      10.0.0.1      <none>        443/TCP           23m
+service/udp-server   LoadBalancer   10.0.144.10   192.161.0.6     20001:30535/UDP   27s
+```
+
+
+
+From the VM, access the Service
+
+```bash
+$ git clone https://github.com/rishasi/udp-client-server.git
+$ cd udp-client-server/client
+$ python3 client.py <LB_IP>
+```
+
+
+
+## Challenges
+
+1. Why is the Service not reachable on the Internal IP?
+2. Once the issue is resolved, initiate the traffic from VM to AKS Service, and take a packet capture on the VM and the AKS Node.
+   - If there are more than 1 AKS Nodes, on which node do we take the PCAP? What are the options we have?
+3. Once the Packet Capture is taken, analyze the PCAP file and draw a Request-Response Flow including as many components as possible.
---
+++ setup-lab-1.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+## Global Variables for defaults
+LOCATION="eastus"
+APPEND=$(openssl rand -hex 4)
+
+## Az Login check
+if $(az account list 2>&1 | grep -q 'az login')
+then
+    echo -e "\n--> Warning: You have to login first with the 'az login' command before you can run this lab tool"
+    az login -o table
+fi
+
+## Create Resource Group
+
+RESOURCE_GROUP=tech-prof-lab-1-${APPEND}
+
+echo "\n--> Creating resource group ${RESOURCE_GROUP}..."
+az group create --name $RESOURCE_GROUP --location $LOCATION -o table 1>/dev/null
+
+sleep 10
+
+# Create 2 VNets: each for CNI and Kubenet
+
+echo "\n--> Creating VNets for 2 AKS Clusters..."
+az network vnet create -g $RESOURCE_GROUP --location $LOCATION \
+-n kubenet-custom-vnet-${APPEND} --address-prefix 192.161.0.0/16 \
+--subnet-name kubenet-custom-subnet-${APPEND} --subnet-prefix 192.161.0.0/24 1>/dev/null
+
+sleep 10
+
+az network vnet create -g $RESOURCE_GROUP --location $LOCATION \
+-n cni-custom-vnet-${APPEND} --address-prefix 192.162.0.0/16 \
+--subnet-name cni-custom-subnet-${APPEND} --subnet-prefix 192.162.0.0/24 1>/dev/null
+
+sleep 10
+
+## Store the Values
+
+KUBENET_VNET_ID=$(az network vnet show -g $RESOURCE_GROUP --name kubenet-custom-vnet-${APPEND} --query id -o tsv)
+KUBENET_SUBNET_ID=$(az network vnet subnet show -g $RESOURCE_GROUP --vnet-name kubenet-custom-vnet-${APPEND} --name kubenet-custom-subnet-${APPEND} --query id -o tsv)
+
+CNI_VNET_ID=$(az network vnet show -g $RESOURCE_GROUP --name cni-custom-vnet-${APPEND} --query id -o tsv)
+CNI_SUBNET_ID=$(az network vnet subnet show -g $RESOURCE_GROUP --vnet-name cni-custom-vnet-${APPEND} --name cni-custom-subnet-${APPEND} --query id -o tsv)
+
+## Create KUBENET Cluster
+
+echo "\n--> Creating AKS Clusters"
+
+az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name kubenet-cluster-${APPEND} \
+    --node-count 2 \
+    --enable-managed-identity \
+    --network-plugin kubenet \
+    --service-cidr 10.0.0.0/16 \
+    --dns-service-ip 10.0.0.10 \
+    --pod-cidr 10.244.0.0/16 \
+    --docker-bridge-address 172.17.0.1/16 \
+    --vnet-subnet-id $KUBENET_SUBNET_ID \
+    --yes 1>/dev/null
+
+sleep 10
+
+## Create CNI Cluster
+az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name cni-cluster-${APPEND} \
+    --node-count 2 \
+    --network-plugin azure \
+    --service-cidr 10.1.0.0/16 \
+    --dns-service-ip 10.1.0.10 \
+    --docker-bridge-address 172.18.0.1/16 \
+    --vnet-subnet-id $CNI_SUBNET_ID \
+    --yes 1>/dev/null
+
+sleep 10
+
+## Create NSG for 2 VMs
+
+echo "\n--> Creating NSG for Custom VMs"
+
+az network nsg create \
+    --name kubenet-vm-nsg \
+    --resource-group $RESOURCE_GROUP 1>/dev/null 
+
+sleep 5
+
+az network nsg rule create --resource-group $RESOURCE_GROUP \
+    --nsg-name kubenet-vm-nsg --name SSHRule \
+    --priority 4096 --source-address-prefixes 0.0.0.0/0 \
+    --source-port-ranges '*' --destination-address-prefixes '*' \
+    --destination-port-ranges 22 80 8080 --access Allow \
+    --protocol Tcp --description "Allow access on port 22, 80 and 8080." 1>/dev/null
+
+az network nsg create \
+    --name cni-vm-nsg \
+    --resource-group $RESOURCE_GROUP 1>/dev/null 
+
+sleep 5
+
+az network nsg rule create --resource-group $RESOURCE_GROUP \
+    --nsg-name cni-vm-nsg --name SSHRule \
+    --priority 4096 --source-address-prefixes 0.0.0.0/0 \
+    --source-port-ranges '*' --destination-address-prefixes '*' \
+    --destination-port-ranges 22 80 8080 --access Allow \
+    --protocol Tcp --description "Allow access on port 22, 80 and 8080." 1>/dev/null
+
+## Create Subnet for 2 VMs
+
+echo "\n--> Creating new Subnets for Custom VMs"
+
+az network vnet subnet create --name kubenet-vm-subnet \
+    --resource-group $RESOURCE_GROUP --vnet-name kubenet-custom-vnet-${APPEND} \
+    --address-prefix 192.161.1.0/24 1>/dev/null 
+
+sleep 10
+
+az network vnet subnet create --name cni-vm-subnet \
+    --resource-group $RESOURCE_GROUP --vnet-name cni-custom-vnet-${APPEND} \
+    --address-prefix 192.162.1.0/24 1>/dev/null 
+
+sleep 10
+
+## Create VM in Kubenet Cluster's VNet, in the new Subnet
+
+echo "\n--> Creating VMs"
+az vm create -n kubenet-vm -g $RESOURCE_GROUP \
+	--image UbuntuLTS \
+	--admin-username azureuser \
+	--admin-password Passw0rd@123 \
+	--subnet kubenet-vm-subnet \
+	--vnet-name kubenet-custom-vnet-${APPEND} \
+	--public-ip-sku Standard \
+	--nsg kubenet-vm-nsg
+
+## Create VM in CNI Cluster's VNet, in the new Subnet
+az vm create -n cni-vm -g $RESOURCE_GROUP \
+	--image UbuntuLTS \
+	--admin-username azureuser \
+	--admin-password Passw0rd@123 \
+	--subnet cni-vm-subnet \
+	--vnet-name cni-custom-vnet-${APPEND} \
+	--public-ip-sku Standard \
+	--nsg cni-vm-nsg
